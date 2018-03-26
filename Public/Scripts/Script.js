8d2486eb8c32759073671b919581a16c6a5995de
rennehir <git@rennehir.fi>
2018-03-26 23:59:52 +0300

    Add counter, timer and results

--- Public/Scripts/Script.js
+++ Public/Scripts/Script.js
@@ -1,33 +1,62 @@
 // -----JS CODE-----
 // @input SceneObject item
-// @input SceneObject[] numbers
+// @input Component.SpriteVisual counter
+// @input Component.SpriteVisual timer
+// @input Component.SpriteVisual resultText
+// @input Component.SpriteVisual result
+// @input Component.SpriteVisual resultBg
 
+var counterControl = script.counter.mainPass.baseTex.control;
+var resControl = script.result.mainPass.baseTex.control;
+var timerControl = script.timer.mainPass.baseTex.control;
 var birdsTapped = 0;
-hideNumbers();
 
-function hideNumbers () {
-    for (var i = 0; i < script.numbers.length; i++) {
-        script.numbers[i].enabled = false;
+var timeoutEvent = script.createEvent("DelayedCallbackEvent");
+timeoutEvent.bind(function(eventData) {
+    timerControl.pause();
+    
+    // Show results
+    script.resultText.enabled = true;
+    script.result.enabled = true;
+    script.resultBg.enabled = true;
+    
+    // Hide timer, counter and item
+    script.timer.enabled = false;
+    script.counter.enabled = false;
+    script.item.enabled = false;
+});
+
+// Hide results
+script.resultText.enabled = false;
+script.result.enabled = false;
+script.resultBg.enabled = false;
+
+// Bind the function to the touch event.
+var touchEvent = script.createEvent("TapEvent");
+touchEvent.bind(moveItem);
+
+// Set counter to zero
+counterControl.pauseAtFrame(0);
+timerControl.pauseAtFrame(0);
+
+// Move bird, increase counter and show the amount of tapped birds
+function moveItem () {
+    if (birdsTapped === 0) {
+        timerControl.play(1, 0.0);
+        timeoutEvent.reset(10);
     }
+    script.item.getTransform().setWorldPosition(randomPosition());
+    birdsTapped++;
+    counterControl.pauseAtFrame(birdsTapped);
+}
+
+function randomPosition () {
+    var x = randomNumber(-25, 25);
+    var y = randomNumber(-25,25);
+    var z = randomNumber(-150, -50);
+    return new vec3(x, y, z);
 }
 
 function randomNumber (min, max) {
     return Math.random()*(max - min) + min;
 }
-
-function randomPosition () {
-    return new vec3(randomNumber(-25, 25), randomNumber(-25,25), randomNumber(-150, -50));
-}
-
-function moveItem () {
-    script.item.getTransform().setWorldPosition(randomPosition());
-    if (birdsTapped < script.numbers.length) {
-        hideNumbers();
-        script.numbers[birdsTapped].enabled = true;
-        birdsTapped++;
-    }
-}
-
-// Bind the function to the touch event.
-var touchEvent = script.createEvent("TapEvent");
-touchEvent.bind(moveItem);
